6bf6568b8f0138092600ee4350895ec6e86ba965
Bumbilo <dimagolyak@gmail.com>
2017-12-25 16:11:03 +0200

    sections

--- metravel.js
+++ metravel.js
@@ -5,7 +5,46 @@ const app = express();
 var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express-handlebars')
-  .create({ defaultLayout: 'main' });
+  .create({ 
+    defaultLayout: 'main',
+    helpers: {
+      section: (name, options) => {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      }
+    }
+});
+
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Портленд',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Сплошная облачность ',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Бенд',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Малооблачно',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Манзанита',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Небольшой дождь',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  }
+}
+
 
 app.engine('handlebars', handlebars.engine);
 
@@ -18,6 +57,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' &&
     req.query.test === '1';
+  next();
+});
+
+app.use(function (req, res, next) {
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
   next();
 });
 
